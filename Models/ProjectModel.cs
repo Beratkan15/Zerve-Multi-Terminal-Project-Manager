767f4b01ef95d26d1a868817844e0044082266e2
Beratkan15 <58602041+Beratkan15@users.noreply.github.com>
2026-01-07 03:47:52 +0300

    Upload Models/ProjectModel.cs

---
+++ Models/ProjectModel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Zerve.Models
+{
+    public class Project : INotifyPropertyChanged
+    {
+        private bool _isRunning;
+
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string CustomId { get; set; } = string.Empty;
+        public string FolderPath { get; set; } = string.Empty;
+        public string Command { get; set; } = string.Empty;
+        
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set
+            {
+                if (_isRunning != value)
+                {
+                    _isRunning = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public Project()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
